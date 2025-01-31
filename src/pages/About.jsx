500228218dca1b7282626a9a06468db4a34ea5c7
Carl Joseph <carljoseph156@gmail.com>
2025-01-30 22:11:15 -0500

    Updated About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h1 className="about-header">About Us</h1>
+      <p className="about-paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A velit facere
+        laborum amet aspernatur, hic odio explicabo labore, iste id voluptates
+        sed voluptas dolorem itaque officia, ipsam dolore illo necessitatibus!
+        Quisquam, magni! Eius magnam, officiis consequuntur ipsum ex, veniam
+        perspiciatis recusandae commodi voluptatum. Minus, earum. Quidem, earum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <br />A voluptatum quia nisi, veniam quos delectus doloribus voluptatum,
+        unde quod neque perspiciatis ea. Quos, quas! Quae voluptatum deleniti
+        aperiam autem, natus commodi molestias earum. Doloribus, obcaecati.
+        Autem voluptas ex recusandae, iure non vel quibusdam nisi. Porro,
+        distinctio? Atque, aspernatur? Facilis, est. Adipisci, cumque? Vitae,
+        molestias. Quam, delectus? Saepe, non? Quod, unde.
+      </p>
+    </>
+  );
 }
 
 export default About;
