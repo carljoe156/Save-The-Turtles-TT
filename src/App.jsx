abd91a575c70a3496fec285ec73bdae430016b53
Carl Joseph <carljoseph156@gmail.com>
2025-01-30 22:13:17 -0500

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+// import Contact from "./pages/Contact";
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <Nav />
-      <HomePage />
+      {/* <HomePage /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
       <Footer />
     </>
   );
